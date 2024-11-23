a5bc4a9307e6c77265c4239167421a874cd11b94
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-11-23 22:04:31 +0200

    update commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-// import authRouter from './auth/auth.js';
+import authRouter from './auth/auth.js';
 import { editUserController } from '../controllers/users.js';
 import { upload } from '../middlewares/multers.js';
 import { currenUserController } from '../controllers/auth.js';
@@ -7,9 +7,8 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { updateUserSchema } from '../validation/user.js';
 const router = new Router();
-const jsonParser = express.json();
 
-// router.use('/auth', authRouter);
+router.use('/auth', authRouter);
 router.use(authenticate);
 router.get('/current', ctrlWrapper(currenUserController));
 router.patch(
@@ -18,6 +17,6 @@ router.patch(
   validateBody(updateUserSchema),
   ctrlWrapper(editUserController),
 );
-// router.patch('editUser', editUserController);
+router.patch('/editUser', editUserController);
 
 export default router;
