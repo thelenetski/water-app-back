77a54f6d82cda1b3a41e84317195f37258ca6731
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-11-29 19:24:14 +0200

    add google-oauth

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,28 +1,42 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
+  getGoogleOAuthUrlController,
+  loginWithGoogleController,
   logoutController,
   refreshUserController,
   signInController,
   signUpController,
-} from '../../controllers/auth.js';
-import { validateBody } from '../../middlewares/validateBody.js';
-import { ctrlWrapper } from '../../utils/ctrlWrapper.js';
-import {signinSchema, signupSchema} from "../../schemas/auth.js";
-import {authenticate} from "../../middlewares/authenticate.js";
+} from "../../controllers/auth.js";
+import { validateBody } from "../../middlewares/validateBody.js";
+import { ctrlWrapper } from "../../utils/ctrlWrapper.js";
+import {
+  loginWithGoogleOAuthSchema,
+  signinSchema,
+  signupSchema,
+} from "../../schemas/auth.js";
+import { authenticate } from "../../middlewares/authenticate.js";
 
 const router = new Router();
 
 router.post(
-  '/signup',
+  "/signup",
   validateBody(signupSchema),
   ctrlWrapper(signUpController),
 );
 
-router.post('/signin', validateBody(signinSchema), signInController);
+router.post("/signin", validateBody(signinSchema), signInController);
 
 router.use(authenticate);
-router.post('/logout', ctrlWrapper(logoutController));
+router.post("/logout", ctrlWrapper(logoutController));
 
-router.post('/refresh', ctrlWrapper(refreshUserController));
+router.post("/refresh", ctrlWrapper(refreshUserController));
+
+router.get("/get-oauth-url", ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  "/confirm-oauth",
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
+);
 
 export default router;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,75 +1,105 @@
-import {Users} from "../models/users.js";
+import { Users } from "../models/users.js";
 import bcrypt from "bcrypt";
+import { randomBytes } from "crypto";
 import createHttpError from "http-errors";
-import {createSession} from "../utils/createSession.js";
-import {Sessions} from "../models/sessions.js";
+import { createSession } from "../utils/createSession.js";
+import { Sessions } from "../models/sessions.js";
+import {
+  getFullNameFromGoogleTokenPayload,
+  validateCode,
+} from "../utils/googleOAuth2.js";
 
 export const signup = async (payload) => {
-    const {email, password} = payload;
+  const { email, password } = payload;
 
-    const isUserExist = await Users.findOne({email: email});
+  const isUserExist = await Users.findOne({ email: email });
 
-    if (isUserExist) throw createHttpError(409, 'User already exists');
+  if (isUserExist) throw createHttpError(409, "User already exists");
 
-    const encryptedPassword = await bcrypt.hash(password, 10);
+  const encryptedPassword = await bcrypt.hash(password, 10);
 
-    return await Users.create({email: email, password: encryptedPassword});
+  return await Users.create({ email: email, password: encryptedPassword });
 };
 
 export const signin = async (payload) => {
-    const {email, password} = payload;
+  const { email, password } = payload;
 
-    const user = await Users.findOne({email: email});
+  const user = await Users.findOne({ email: email });
 
-    if (!user) throw createHttpError(404, 'User not found');
+  if (!user) throw createHttpError(404, "User not found");
 
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) throw createHttpError(404, "Passwords do not match");
 
-    if (!isPasswordValid) throw createHttpError(404, 'Passwords do not match');
+  await Sessions.deleteOne({ userId: user._id });
 
-    await Sessions.deleteOne({userId: user._id});
+  const session = createSession();
 
-    const session = createSession();
-
-    return Sessions.create({userId: user._id, ...session});
+  return Sessions.create({ userId: user._id, ...session });
 };
 
 export const logout = async (payload) => {
-    const {_id} = payload;
+  const { _id } = payload;
 
-    const session = await Sessions.findOne({userId: _id});
+  const session = await Sessions.findOne({ userId: _id });
 
-    if (!session) throw createHttpError(404, 'User not found');
+  if (!session) throw createHttpError(404, "User not found");
 
-    await Sessions.deleteOne({userId: _id});
+  await Sessions.deleteOne({ userId: _id });
 };
 
 export const getCurrentUser = async (payload) => {
-    const {_id} = payload;
+  const { _id } = payload;
 
-    const user = await Users.findById(_id);
+  const user = await Users.findById(_id);
 
-    if (!user) throw createHttpError(404, 'User not found');
+  if (!user) throw createHttpError(404, "User not found");
 
-    return user;
-}
+  return user;
+};
 
 export const refreshUser = async (payload) => {
-    const {_id} = payload;
+  const { _id } = payload;
 
-    const session = await Sessions.findOne({userId:_id});
+  const session = await Sessions.findOne({ userId: _id });
 
-    if (!session) throw createHttpError(404, 'User not found');
+  if (!session) throw createHttpError(404, "User not found");
 
-    if (new Date() > session.accessTokenValidUntil) throw new createHttpError(401, 'Session expired');
+  if (new Date() > session.accessTokenValidUntil)
+    throw new createHttpError(401, "Session expired");
 
-    await Sessions.deleteOne({_id: session._id});
+  await Sessions.deleteOne({ _id: session._id });
 
-    const newSession = createSession();
+  const newSession = createSession();
 
-    return await Sessions.create({
-        userId: session.userId,
-        ...newSession,
+  return await Sessions.create({
+    userId: session.userId,
+    ...newSession,
+  });
+};
+
+/*-------------------GOOGLE AUTH--------------------*/
+export const loginOrSignupWithGoogle = async (code) => {
+  const loginTicket = await validateCode(code);
+  const payload = loginTicket.getPayload();
+  if (!payload) throw createHttpError(401);
+
+  let user = await Users.findOne({ email: payload.email });
+  if (!user) {
+    const password = await bcrypt.hash(randomBytes(10), 10);
+    user = await Users.create({
+      email: payload.email,
+      name: getFullNameFromGoogleTokenPayload(payload),
+      password,
+      role: "parent",
     });
+  }
+
+  const newSession = createSession();
+
+  return await Sessions.create({
+    userId: user._id,
+    ...newSession,
+  });
 };
