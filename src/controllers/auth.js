4dc49b8ebe18ec0cf91a1b2c3bc176125c5d586b
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-12-08 17:52:28 +0200

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -51,7 +51,7 @@ export const logoutController = ctrlWrapper(async (req, res) => {
 });
 
 export const refreshUserController = ctrlWrapper(async (req, res) => {
-  console.log(req);
+  console.log('***************REFRESH********************', req.cookies);
   const session = await refreshUser(req.cookies);
 
   res.cookie("sessionId", session._id, {
